cb48207f7c98c0b9576de0d5b8517320528f17b4
Reno Kurniawan <42386394+reoniwan@users.noreply.github.com>
2019-12-19 08:59:37 +0700

    fill host, user, pass, and db

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "reoneappserver.database.windows.net";
+    $user = "reone";
+    $pass = "Admin0108";
+    $db = "reonedb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
